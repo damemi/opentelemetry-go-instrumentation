aa8b8caf5307172a9fb0beddb5b4f77d7e2210a5
Mike Dame <mikedame@google.com>
2023-02-21 15:36:34 +0000

    Add debug statements

--- offsets-tracker/downloader/go_wrapper.go
+++ offsets-tracker/downloader/go_wrapper.go
@@ -3,10 +3,11 @@ package downloader
 import (
 	_ "embed"
 	"fmt"
-	"github.com/keyval-dev/offsets-tracker/utils"
 	"io/fs"
 	"io/ioutil"
 	"path"
+
+	"github.com/keyval-dev/offsets-tracker/utils"
 )
 
 const appName = "testapp"
@@ -20,28 +21,33 @@ var (
 )
 
 func DownloadBinary(modName string, version string) (string, string, error) {
+	fmt.Println("Creating temp dir")
 	dir, err := ioutil.TempDir("", appName)
 	if err != nil {
 		return "", "", err
 	}
 
+	fmt.Println("Writing testapp go.mod")
 	goModContent := fmt.Sprintf(goMod, modName, version)
 	err = ioutil.WriteFile(path.Join(dir, "go.mod"), []byte(goModContent), fs.ModePerm)
 	if err != nil {
 		return "", "", err
 	}
 
+	fmt.Println("Writing testapp main.go")
 	goMainContent := fmt.Sprintf(goMain, modName)
 	err = ioutil.WriteFile(path.Join(dir, "main.go"), []byte(goMainContent), fs.ModePerm)
 	if err != nil {
 		return "", "", err
 	}
 
+	fmt.Println("Running go mod tidy")
 	err, _, _ = utils.RunCommand("go mod tidy -compat=1.17", dir)
 	if err != nil {
 		return "", "", err
 	}
 
+	fmt.Println("Running go build")
 	err, _, _ = utils.RunCommand("GOOS=linux GOARCH=amd64 go build", dir)
 	if err != nil {
 		return "", "", err
